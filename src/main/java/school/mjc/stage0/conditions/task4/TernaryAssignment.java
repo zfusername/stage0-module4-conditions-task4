34a8a957c469e47331bb005c22cdca824baa0301
zfusername <farrukh_ziyaev@epam.com>
2023-07-27 15:52:08 +0500

    change

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -3,5 +3,6 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
         int result = first > second ? 10 : -10;
+        System.out.println(result);
     }
 }
